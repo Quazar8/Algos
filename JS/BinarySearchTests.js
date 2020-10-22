8a0aaf102aa4f979ccea02838c83452d0fc0f5cc
Quazar8 <junkyard12d@gmail.com>
2020-10-22 21:51:36 +0300

    Refactoring binarySearch unit tests
    Improving the flexibility of the unit tests so that they can be used
    with different arrays and safeguard from writing repetitive code.

--- JS/BinarySearchTests.js
+++ JS/BinarySearchTests.js
@@ -1,28 +1,31 @@
 const binarySearch = require('./BinarySearch')
 const assert = require('assert')
 let arrOdd = [-12, -2, -1, 0, 1, 4, 5, 17, 19]
-//           0   1   2  3  4  5  6  7   8
 let arrEven = [-3, -1, 0, 2, 34, 65]
-//              0   1  2  3  4   5  
+
 describe('Binary Search', () => {
-    describe('Array with even number of members', () => {
-        let last = arrEven.length - 1
+    describe('Array with even number of members', arrayTests(arrEven))
+})
+
+function arrayTests(arr) {
+    return () => {
+        let last = arr.length - 1
         let mid = Math.floor(last / 2)
         it('should return index in the middle', () => {
-            assert.strict.equal(binarySearch(arrEven, arrEven[mid]), mid)
+            assert.strict.equal(binarySearch(arr, arr[mid]), mid)
         })
         it('should return middle index + 1', () => {
-            assert.strict.equal(binarySearch(arrEven, arrEven[mid + 1]), mid + 1)
+            assert.strict.equal(binarySearch(arr, arr[mid + 1]), mid + 1)
         })
 
         it('should return middle index - 1', () => {
-            assert.strict.equal(binarySearch(arrEven, arrEven[mid - 1]), mid - 1)
+            assert.strict.equal(binarySearch(arr, arr[mid - 1]), mid - 1)
         })
         it('should return last index', () => {
-            assert.strict.equal(binarySearch(arrEven, arrEven[last]), last)
+            assert.strict.equal(binarySearch(arr, arr[last]), last)
         })
         it('should return first index', () => {
-            assert.strict.equal(binarySearch(arrEven, arrEven[0]), 0)
+            assert.strict.equal(binarySearch(arr, arr[0]), 0)
         })
-    })
-})
+    }
+}
