dc1efcccf373e8e841abf5992333f4943a3f77a2
Quazar8 <junkyard12d@gmail.com>
2020-10-22 19:29:14 +0300

    Adding first unit test for binary search

--- JS/BinarySearchTests.js
+++ JS/BinarySearchTests.js
@@ -1,5 +1,13 @@
 const binarySearch = require('./BinarySearch')
-let arr = [-12, -2, -1, 0, 1, 4, 5, 17, 19]
+const assert = require('assert')
+let arrOdd = [-12, -2, -1, 0, 1, 4, 5, 17, 19]
 //           0   1   2  3  4  5  6  7   8
-
-console.log(binarySearch(arr, 0))
+let arrEven = [-3, -1, 0, 2, 34, 65]
+//              0   1  2  3  4   5  
+describe('Binary Search', () => {
+    describe('Array with even number of members', () => {
+        it('should return index in the middle #1', () => {
+            assert.strict.equal(binarySearch(arrEven, 0), 2)
+        })
+    })
+})
