832d808e56b5d8098aeca9ddeed5aa77c855ebc8
Quazar8 <junkyard12d@gmail.com>
2020-10-22 19:46:32 +0300

    adding unit tests for binary search
    unit tests testing the algorithm on an array with even count of members

--- JS/BinarySearchTests.js
+++ JS/BinarySearchTests.js
@@ -6,8 +6,23 @@ let arrEven = [-3, -1, 0, 2, 34, 65]
 //              0   1  2  3  4   5  
 describe('Binary Search', () => {
     describe('Array with even number of members', () => {
-        it('should return index in the middle #1', () => {
-            assert.strict.equal(binarySearch(arrEven, 0), 2)
+        let last = arrEven.length - 1
+        let mid = Math.floor(last / 2)
+        it('should return index in the middle', () => {
+            assert.strict.equal(binarySearch(arrEven, arrEven[mid]), mid)
+        })
+        it('should return middle index + 1', () => {
+            assert.strict.equal(binarySearch(arrEven, arrEven[mid + 1]), mid + 1)
+        })
+
+        it('should return middle index - 1', () => {
+            assert.strict.equal(binarySearch(arrEven, arrEven[mid - 1]), mid - 1)
+        })
+        it('should return last index', () => {
+            assert.strict.equal(binarySearch(arrEven, arrEven[last]), last)
+        })
+        it('should return first index', () => {
+            assert.strict.equal(binarySearch(arrEven, arrEven[0]), 0)
         })
     })
 })
