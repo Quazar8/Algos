a9ccc43161bf60eaefdd9a78492a87904e2ccf03
Quazar8 <junkyard12d@gmail.com>
2020-10-25 20:18:54 +0200

    Including second binary search method in the tests
    Making the arrayTest function more flexible by enabling methods to be
    passed as parameters and thus using the same function for the two methods

--- JS/BinarySearchTests.js
+++ JS/BinarySearchTests.js
@@ -9,34 +9,41 @@ let floatArr = [-0.2, -0.1, 0.0, 2.3, 2.5]
 let mixed = [-3, -2.7, -1, 1, 2.3, 3.5, 6, 7.5]
 
 describe('Binary Search', () => {
-    describe('Array with even number of elements', arrayTests(arrEven))
-    describe('Array with odd number of elements', arrayTests(arrOdd))
-    describe('Array with floating point numbers', arrayTests(floatArr))
-    describe('Array with mixed decimal and whole numbers', arrayTests(mixed))
+    describe('Array with even number of elements', arrayTests(arrEven, binarySearch))
+    describe('Array with odd number of elements', arrayTests(arrOdd, binarySearch))
+    describe('Array with floating point numbers', arrayTests(floatArr, binarySearch))
+    describe('Array with mixed decimal and whole numbers', arrayTests(mixed, binarySearch))
 })
 
-function arrayTests(arr) {
+describe('Binary Search Second Method', () => {
+    describe('Array with even number of elements', arrayTests(arrEven, binarySearchSecond))
+    describe('Array with odd number of elements', arrayTests(arrOdd, binarySearchSecond))
+    describe('Array with floating point numbers', arrayTests(floatArr, binarySearchSecond))
+    describe('Array with mixed decimal and whole numbers', arrayTests(mixed, binarySearchSecond))
+})
+
+function arrayTests(arr, method) {
     return () => {
         let last = arr.length - 1
         let mid = Math.floor(last / 2)
         it('should return index in the middle', () => {
-            assert.strict.equal(binarySearch(arr, arr[mid]), mid)
+            assert.strict.equal(method(arr, arr[mid]), mid)
         })
         it('should return middle index + 1', () => {
-            assert.strict.equal(binarySearch(arr, arr[mid + 1]), mid + 1)
+            assert.strict.equal(method(arr, arr[mid + 1]), mid + 1)
         })
 
         it('should return middle index - 1', () => {
-            assert.strict.equal(binarySearch(arr, arr[mid - 1]), mid - 1)
+            assert.strict.equal(method(arr, arr[mid - 1]), mid - 1)
         })
         it('should return last index', () => {
-            assert.strict.equal(binarySearch(arr, arr[last]), last)
+            assert.strict.equal(method(arr, arr[last]), last)
         })
         it('should return first index', () => {
-            assert.strict.equal(binarySearch(arr, arr[0]), 0)
+            assert.strict.equal(method(arr, arr[0]), 0)
         })
         it('should return -1 when element is missing', () => {
-            assert.strict.equal(binarySearch(arr, MAX_NUMBER + 1), -1)
+            assert.strict.equal(method(arr, MAX_NUMBER + 1), -1)
         })
     }
 }
